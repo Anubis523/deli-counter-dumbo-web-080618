a8f6fdd8dbb2e141cc3e776ea18a4c52d5dbd168
Kevin Steele <kevindsteeleii@gmail.com>
2018-07-29 14:20:15 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,5 +18,5 @@ end
 
 def now_serving (deli)
   puts deli.empty? "There is nobody waiting to be served!" 
-    : puts "Currently serving #{deli.shift}"
+    :"Currently serving #{deli.shift}"
 end
